18414ecdbb97dfcec6988474108fd2f046c882c0
wangxuedong <15904444120@163.com>
2020-08-30 02:07:53 +0800

    444

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
  * @Version: 1.0
  * @Autor: fortnights
  * @Date: 2020-08-30 00:54:07
- * @LastEditTime: 2020-08-30 01:04:28
+ * @LastEditTime: 2020-08-30 02:07:33
  */
 #include <stdio.h>
 int main()
@@ -11,6 +11,7 @@ int main()
     printf("%d, %d\n", increment(10), negate(10));
     printf("%d, %d\n", increment(0), negate(0));
     printf("%d, %d\n", increment(-10), negate(-10));
+
     system("pause");
     return 0;
 }
