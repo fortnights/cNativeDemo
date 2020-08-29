7113838f537c52aa8d807f5e3526cad90c1b813e
wangxuedong <15904444120@163.com>
2020-08-30 03:15:29 +0800

    rear

---
+++ rearrang.c
@@ -0,0 +1,19 @@
+/*
+ * @Author: your name
+ * @Date: 2020-08-24 22:51:48
+ * @LastEditTime: 2020-08-24 23:03:59
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \cNativeDemo\rearrang.c
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_COLS 20
+#define MAX_INPUT 1000
+
+int read_column_numbers(int column[], int max);
+void rearrange(char const *input, char *output, int const columns[], int n_columns);
+int main()
+{
+    return 0;
+}
