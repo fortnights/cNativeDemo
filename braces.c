f08b60b19d75fd53c54b5b38946429f90acf97d6
wangxuedong <15904444120@163.com>
2020-08-30 02:04:19 +0800

    333

--- braces.c
+++ braces.c
@@ -3,7 +3,7 @@
  * @Version: 1.0
  * @Autor: fortnights
  * @Date: 2020-08-30 01:12:33
- * @LastEditTime: 2020-08-30 02:00:20
+ * @LastEditTime: 2020-08-30 02:04:05
  * @LastEditTime: 2020-08-30 01:52:57
  */
 #include <stdio.h>
@@ -37,6 +37,5 @@ int main()
         printf("右括号匹配失败！");
     }
 
-    system("pause");
     return EXIT_SUCCESS;
 }
